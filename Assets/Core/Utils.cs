403df5f149a2fe4d46d994c97e099ac3104ae674
Florian <florian.aurousseau@protonmail.com>
2023-02-03 23:03:08 +0100

    Supercopter rotation & Rotation Util

--- Assets/Core/Utils.cs
+++ Assets/Core/Utils.cs
@@ -10,5 +10,15 @@ namespace Core
             var direction = (b - a).normalized;
             return direction.x > 0 ? 1 : -1;
         }
+
+        public static Quaternion GetYRotationFromXDirection(float xDirection, Quaternion rotation) { return GetYRotationFromXDirection(xDirection, rotation.eulerAngles); }
+
+        public static Quaternion GetYRotationFromXDirection(float xDirection, Vector3 rotation)
+        {
+            return Quaternion.Euler(xDirection > 0 
+                ? new Vector3(rotation.x, 0, rotation.z) 
+                : new Vector3(rotation.x, -180, rotation.z)
+            );
+        }
     }
 }
