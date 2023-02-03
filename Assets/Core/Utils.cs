d41a304274f2c98ddd836a6f67bcfa9b8a585f21
Florian <florian.aurousseau@protonmail.com>
2023-02-03 22:44:19 +0100

    Tools

---
+++ Assets/Core/Utils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class Utils
+    {
+        public static float GetXDirection(Transform a, Transform b) { return GetXDirection(a.position, b.position); }
+        public static float GetXDirection(Vector2 a, Vector2 b)
+        {
+            var direction = (b - a).normalized;
+            return direction.x > 0 ? 1 : -1;
+        }
+    }
+}
---
+++ Assets/Props/Supercopter/Scripts/Supercopter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Core;
+using UnityEngine;
+
+namespace Props.Supercopter.Scripts
+{
+    public class Supercopter : MonoBehaviour
+    {
+        [SerializeField] private Transform targetTransform;
+        
+        [Header("Config")]
+        [SerializeField] private float speed = 0.5f;
+        [SerializeField] private float closestDistanceFromTarget = 3.5f;
+        [SerializeField] private float shootCooldown = 5.0f;
+
+        private void Update()
+        {
+            if(!IsAtClosestDistance())
+                UpdatePosition();
+        }
+
+        private bool IsAtClosestDistance()
+        {
+            return Math.Abs(transform.position.x - targetTransform.position.x) < closestDistanceFromTarget;
+        }
+        
+        
+        
+        private void UpdatePosition()
+        {
+            var position = transform.position;
+            var xDirection = Utils.GetXDirection(position, targetTransform.position);
+            var newX = position.x + (xDirection * (speed * Time.deltaTime));
+            
+            transform.position = new Vector2(newX, position.y);
+        }
+    }
+}
