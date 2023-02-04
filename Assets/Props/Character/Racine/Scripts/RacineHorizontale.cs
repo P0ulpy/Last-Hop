5f8b059942421e46b148ebc5577b408396409718
Valentin <valentin.barrere@ynov.com>
2023-02-04 13:08:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Props/Character/Racine/Scripts/RacineHorizontale.cs
+++ Assets/Props/Character/Racine/Scripts/RacineHorizontale.cs
@@ -11,17 +11,24 @@ public class RacineHorizontale : MonoBehaviour
      public enum Direction {Right, Left};
     [SerializeField] private GameObject racineVerticale;
     [SerializeField] private SpriteRenderer _rootSprite;
-    [SerializeField] private GameObject _spriteMask;
+    [SerializeField] private GameObject _Mask;
 
     private int sideSign = 0;
     private float offset = 0;
     private bool Start = true;
     private bool Stop = false;
-    private Vector2 m_startOfRoot = new Vector3(0, 0, 0);
+    private Vector3 m_startOfRoot = new Vector3(0, 0, 0);
     public float speed = 6;
     public float hauteur = -1;
     
     // Update is called once per frame
+
+    private void Awake()
+    {
+        //  _rootSprite.transform.localScale = new Vector3(_rootSprite.transform.localScale.x * 10, 1, 1);
+      //  speed = 1;
+    }
+
     void Update()
     {
         if (Start)
@@ -45,17 +52,14 @@ public class RacineHorizontale : MonoBehaviour
             Start = false;
         }
 
-        if (!Stop) _spriteMask.transform.localPosition = new Vector3(_spriteMask.transform.localPosition.x + (sideSign * speed * Time.deltaTime), 0, 0);
-
-
+       if (!Stop) _Mask.transform.localPosition = new Vector3(_Mask.transform.localPosition.x + (sideSign * speed * Time.deltaTime), 0, 0);
     }
-
-
+    
     public void StopAimingThenShoot()
     {
         Stop = true;
         
-        Instantiate(racineVerticale, new Vector3(offset + _spriteMask.transform.position.x,hauteur,0) , Quaternion.identity);
+        Instantiate(racineVerticale, new Vector3(_Mask.transform.position.x ,hauteur,0) , Quaternion.identity);
     }
 
     public void StartAiming(Direction myDir)
@@ -63,18 +67,18 @@ public class RacineHorizontale : MonoBehaviour
         if (myDir == Direction.Right)
         {
             m_startOfRoot = new Vector3((_rootSprite.bounds.size.x / 2), hauteur, 0);
-            sideSign = 1;
             offset = -_rootSprite.bounds.size.x;
+            sideSign = 1;
         }
         else if(myDir == Direction.Left)
         {
-            sideSign = -1;
             m_startOfRoot = new Vector3(-(_rootSprite.bounds.size.x / 2), hauteur, 0);
             offset = 0;
+            sideSign = -1;
+            _Mask.transform.Rotate(new Vector3(0,0,180));
+            _Mask.transform.localPosition =
+                new Vector3(_Mask.transform.localPosition.x + _rootSprite.bounds.size.x, hauteur, 0);
         }
-        
-        
-        _spriteMask.transform.localPosition = sideSign*m_startOfRoot;
         this.transform.position = new Vector3(m_startOfRoot.x,hauteur,0); 
     }
 
--- Assets/Props/Character/Racine/Scripts/RacineVerticale.cs
+++ Assets/Props/Character/Racine/Scripts/RacineVerticale.cs
@@ -10,19 +10,20 @@ public class RacineVerticale : MonoBehaviour
 {
 
     [SerializeField] private SpriteRenderer _rootSprite;
-
-
-    private bool Stop = false;
-    private Vector2 m_startOfRoot = new Vector3(0, 0, 0);
+    
+    
     public float speed = 5;
     public float hauteur = 0;
-    private float Spawnx = 0.0f;
     public float offsetY = 1;
-
+    public float hauteurMax = 18;
+    
+    private bool Stop = false;
+    private Vector2 m_startOfRoot = new Vector3(0, 0, 0);
+    private float Spawnx = 0.0f;
     private Vector3 startVector;
     private Vector3 endVector;
     private float time;
-    private float duration;
+    public float duration;
 
 
     private void Awake()
@@ -35,15 +36,14 @@ public class RacineVerticale : MonoBehaviour
     private void Start()
     {
         startVector = transform.position;
-        endVector = new Vector3(transform.position.x, transform.position.y + 18, transform.position.z);
+        endVector = new Vector3(transform.position.x, transform.position.y + hauteurMax, transform.position.z);
         time = 0;
-        duration = 2;
+        duration = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // transform.position = new Vector3(transform.position.x, transform.position.y + (speed * Time.deltaTime) , 0);
         transform.position = Vector3.Lerp(startVector, endVector, time / duration);
         time += Time.deltaTime;
     }
